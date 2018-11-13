716aeb95d32eeff0a40d02ccd3ea5abd2005572d
Guillermo Zerpa <guillermo.zerpa@hotmail.com>
2018-11-13 15:58:14 -0500

    Completed TeachJobsConsole Initial Assigment

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -57,13 +57,16 @@ namespace TechJobsConsole
                     // What is their search term?
                     Console.WriteLine("\nSearch term: ");
                     string searchTerm = Console.ReadLine();
+                    //Make searchTerm Case insensitive
+                    searchTerm = searchTerm.ToLower();
 
                     List<Dictionary<string, string>> searchResults;
 
                     // Fetch results
                     if (columnChoice.Equals("all"))
-                    {
-                        Console.WriteLine("Search all fields not yet implemented.");
+                    {                        
+                        searchResults = JobData.FindByValue(searchTerm);
+                        PrintJobs(searchResults);
                     }
                     else
                     {
@@ -118,7 +121,31 @@ namespace TechJobsConsole
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("printJobs is not implemented yet");
+            //Console.WriteLine("printJobs is not implemented yet");
+
+            if (someJobs.Count > 0)
+            {
+                foreach (Dictionary<string, string> job in someJobs)
+                {
+                    Console.WriteLine("\n*****");
+                    foreach (KeyValuePair<string, string> attribute in job)
+                    {
+                        Console.WriteLine(string.Format("{0}: {1}", attribute.Key, attribute.Value));                    
+                    }
+                    Console.WriteLine("*****");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your request didn't return any result");
+            }
+                                         
+
+
+
         }
+
+
+
     }
 }
